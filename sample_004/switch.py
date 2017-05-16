b9c5d040f10e06534b6c9941365044750d76ab4b
meusebio.depaz <meusebio.depaz@bbva.com>
2017-05-16 12:58:51 +0200

    swap sample

---
+++ sample_004/switch.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# swap manual
+a = 1
+b = 2
+print "a=%d b=%d" % (a, b)
+
+c = a
+a = b
+b = c
+
+print "Tras el swap"
+print "a=%d b=%d" % (a, b)
+
+# Alternativa de python :)
+a, b = b, a
+
+print "Tras el swap automágico"
+print "a=%d b=%d" % (a, b)
